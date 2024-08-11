d6d0f2eef8273a97d3d55b05758b1a3f74ffb833
djleo70 <83383164+djleo70@users.noreply.github.com>
2024-08-11 12:00:27 +0200

    Update Add_ps1_content_in_this.bat

--- Add_ps1_content_in_this.bat
+++ Add_ps1_content_in_this.bat
@@ -1,5 +1,5 @@
 <# :
-    @echo off & chcp 65001 >nul & cd /d "%~dp0"
+    @echo off & chcp 65001 >nul & cd /d "%~dp0" & Title PS1 Batch Wrapper 
 
     ::========= SETTINGS =========
     set "Powershell_WindowStyle=Normal"  :: Normal, Hidden, Minimized, Maximized
